1aaa4709a07712a16ceef3be5c1f7e344d7c8fb1
Marius Teodorescu <mariusteodorescu19@gmail.com>
2021-03-07 11:11:04 +0200

    Changed main page

--- Client/maat/src/App.tsx
+++ Client/maat/src/App.tsx
@@ -3,29 +3,130 @@ import "./App.css";
 
 function App() {
 	return (
-		<div className="bg-gray-50">
-			<div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-				<h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl font-body">
-					<span className="block">Ready to dive in?</span>
-					<span className="block text-indigo-600">Start your free trial today.</span>
-				</h2>
-				<div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-					<div className="inline-flex rounded-md shadow">
-						<a
-							href="#"
-							className="font-body inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-						>
-							Get started
+		<div className="min-h-screen bg-home-page bg-cover bg-center">
+			<div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+				<nav
+					className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+					aria-label="Global"
+				>
+					<div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+						<div className="flex items-center justify-between w-full md:w-auto">
+							<a href="#">
+								<span className="sr-only">Workflow</span>
+							</a>
+							<div className="-mr-2 flex items-center md:hidden">
+								<button
+									type="button"
+									className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+									aria-expanded="false"
+								>
+									<span className="sr-only">Open main menu</span>
+									<svg
+										className="h-6 w-6"
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+										aria-hidden="true"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="2"
+											d="M4 6h16M4 12h16M4 18h16"
+										/>
+									</svg>
+								</button>
+							</div>
+						</div>
+					</div>
+					<div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+						<a className="font-bold font-body text-gray-500 text-5xl">Ma'at</a>
+						<a href="#" className="font-body font-medium text-gray-500 hover:text-gray-300">
+							Product
+						</a>
+
+						<a href="#" className="font-body font-medium text-gray-500 hover:text-gray-900">
+							Features
+						</a>
+
+						<a href="#" className="font-body font-medium text-gray-500 hover:text-gray-900">
+							Marketplace
+						</a>
+
+						<a href="#" className="font-body font-medium text-gray-500 hover:text-gray-900">
+							Company
+						</a>
+
+						<a href="#" className="font-body font-medium text-indigo-600 hover:text-indigo-500">
+							Log in
 						</a>
 					</div>
-					<div className="ml-3 inline-flex rounded-md shadow">
+				</nav>
+			</div>
+			<div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+				<div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+					<div className="px-5 pt-4 flex items-center justify-between">
+						<div></div>
+						<div className="-mr-2">
+							<button
+								type="button"
+								className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+							>
+								<span className="sr-only">Close main menu</span>
+								<svg
+									className="h-6 w-6"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									aria-hidden="true"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M6 18L18 6M6 6l12 12"
+									/>
+								</svg>
+							</button>
+						</div>
+					</div>
+					<div className="px-2 pt-2 pb-3 space-y-1">
 						<a
 							href="#"
-							className="font-body inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+							className="font-body block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
 						>
-							Learn more
+							Product
+						</a>
+
+						<a
+							href="#"
+							className="font-body block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+						>
+							Features
+						</a>
+
+						<a
+							href="#"
+							className="font-body block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+						>
+							Marketplace
+						</a>
+
+						<a
+							href="#"
+							className="font-body block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+						>
+							Company
 						</a>
 					</div>
+					<a
+						href="#"
+						className="font-body block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+					>
+						Log in
+					</a>
 				</div>
 			</div>
 		</div>
--- Client/maat/tailwind.config.js
+++ Client/maat/tailwind.config.js
@@ -1,17 +1,20 @@
-const colors = require("tailwindcss/colors");
+// const colors = require("tailwindcss/colors");
 
 module.exports = {
-    purge: [".src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            fontFamily: {
-                body: ["Inter"],
-            },
-        },
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+	purge: [".src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				body: ["Inter"],
+			},
+			backgroundImage: (theme) => ({
+				"home-page": "url('/src/images/abigail-keenan-8-s5QuUBtyM-unsplash.jpg')",
+			}),
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
