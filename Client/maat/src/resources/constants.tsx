58d9acaf30c8fab3b538d8df4cd8cf5878980e50
Marius Teodorescu <mariusteodorescu19@gmail.com>
2021-06-15 10:30:25 +0300

    Configurations

--- Client/maat/src/resources/constants.tsx
+++ Client/maat/src/resources/constants.tsx
@@ -1,2 +1,2 @@
 export let url =
-	process.env.NODE_ENV === "development" ? "http://localhost:57571" : "https://maatapi.azurewebsites.net";
+	process.env.NODE_ENV === "development" ? "http://localhost:57571" : "https://maatapimanagement.azure-api.net";
